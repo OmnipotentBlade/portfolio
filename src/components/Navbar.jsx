55e2077bdd4875a1cffa69cb9e8c898e9e379021
OmnipotentBlade <91920612+OmnipotentBlade@users.noreply.github.com>
2023-07-12 11:21:19 -0400

    Added mobile navbar transitions/animations

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 'use client'
 import Link from 'next/link';
 import { FaBars } from 'react-icons/fa'
+import { GrClose } from 'react-icons/gr'
 import { useEffect, useState } from 'react';
 
 const NavBar = ({page}) => {
@@ -24,47 +25,50 @@ const NavBar = ({page}) => {
     );
 
     return (
-        <div className={`${page!=='Home' ? null : 'fixed'} top-0 left-0 w-full z-[999] ${isScrolled ? 'backdrop-blur-[10px]' : ''} ${nav ? 'bg-zinc-900 md:bg-transparent' : ''}`}>
-            {/* Mobile */}
-            {nav ? 
-                <div className='fixed md:hidden left-0 top-14 w-[65%] h-80 bg-zinc-900/90'>
-                    <div className='p-4 text-white font-mono'>
-                        <ul className='p-2 pb-3'>
-                            <span className='text-gray-400'>About</span>
-                            <li className='pt-2 pl-4'>
-                                <Link href='/' className={`relative ${page==='Home' ? 'text-blue-200' : ''}`}>
-                                    Home
-                                    <span className='absolute opacity-0 transition-opacity'>_</span>
-                                </Link>
-                            </li>
-                            <li className='pt-2 pl-4'>
-                                <Link href='/skills-experience' className={`group hover:brightness-90 relative ${page==='Skills/Experience' ? 'text-blue-200' : ''}`}>
-                                    Skills/Experience
-                                    <span className='absolute opacity-0 group-hover:opacity-100 transition-opacity'>_</span>
-                                </Link>
-                            </li>
-                            <li className='pt-2 pl-4'>
-                                <Link href='/projects' className={`group hover:brightness-90 relative ${page==='Projects' ? 'text-blue-200' : ''}`}>
-                                    Projects
-                                    <span className='absolute opacity-0 group-hover:opacity-100 transition-opacity'>_</span>
-                                </Link>
-                            </li>
-                        </ul>
-                        <ul className='p-2 pt-3'>
-                            <span className='text-gray-400'>Contant Me</span>
-                            <li className='pt-2 pl-4'>
-                                <Link href='/contact' className={`group hover:brightness-90 relative ${page==='Contact' ? 'text-blue-200' : ''}`}>
-                                    Contact
-                                    <span className='absolute opacity-0 group-hover:opacity-100 transition-opacity'>_</span>
-                                </Link>
-                            </li>
-                        </ul>
-                    </div>
+        <div className={`${page!=='Home' ? null : 'fixed'} top-0 left-0 w-full z-[999] ${isScrolled ? 'backdrop-blur-[10px]' : ''} ${nav ? 'bg-zinc-900 md:bg-transparent transition-all' : 'transition-all'}`}>
+            {/* Mobile Menu */}
+            <div className={`fixed left-0 top-[62px] w-[65%] h-72 bg-zinc-900/90 ${nav ? 'flex transition-opacity' : 'opacity-0 transition-opacity'}`}>
+                <div className={`p-4 text-white font-mono ${nav ? '' : 'hidden'}`}>
+                    <ul className='p-2 pb-3'>
+                        <span className='text-gray-400'>About</span>
+                        <li className='pt-2 pl-4'>
+                            <Link href='/' className={`relative ${page==='Home' ? 'text-blue-200' : ''}`}>
+                                Home
+                                <span className='absolute opacity-0 transition-opacity'>_</span>
+                            </Link>
+                        </li>
+                        <li className='pt-2 pl-4'>
+                            <Link href='/skills-experience' className={`group hover:brightness-90 relative ${page==='Skills/Experience' ? 'text-blue-200' : ''}`}>
+                                Skills/Experience
+                                <span className='absolute opacity-0 group-hover:opacity-100 transition-opacity'>_</span>
+                            </Link>
+                        </li>
+                        <li className='pt-2 pl-4'>
+                            <Link href='/projects' className={`group hover:brightness-90 relative ${page==='Projects' ? 'text-blue-200' : ''}`}>
+                                Projects
+                                <span className='absolute opacity-0 group-hover:opacity-100 transition-opacity'>_</span>
+                            </Link>
+                        </li>
+                    </ul>
+                    <ul className='p-2 pt-3'>
+                        <span className='text-gray-400'>Contact Me</span>
+                        <li className='pt-2 pl-4'>
+                            <Link href='/contact' className={`group hover:brightness-90 relative ${page==='Contact' ? 'text-blue-200' : ''}`}>
+                                Contact
+                                <span className='absolute opacity-0 group-hover:opacity-100 transition-opacity'>_</span>
+                            </Link>
+                        </li>
+                    </ul>
                 </div>
-            : null}
+            </div>
+            {/* Mobile Navbar */}
             <div className='md:hidden flex font-mono p-4'>
                 <div onClick={handleNav} className='hover:brightness-90 ease-in'>
-                    <FaBars size={30} className='invert'/>
+                    {nav ? 
+                        <GrClose size={30} className='invert'/>
+                    :
+                        <FaBars size={30} className='invert'/>
+                    }
                 </div>
                 <div className='flex pt-[3px] pl-3 tracking-wide'>
                     <span className='px-1 pr-2 text-cyan-300'>Ahmed Osman</span>
