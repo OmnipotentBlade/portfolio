74b95253ba3acbc5c8aa5f5d1fa160c93e1b9f94
OmnipotentBlade <91920612+OmnipotentBlade@users.noreply.github.com>
2023-07-11 23:19:59 -0400

    Attempt 3 - Mobile background fix

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -10,12 +10,14 @@ const Hero = ({heading, desc}) => {
             <div className='flex items-center justify-center h-screen'>
                 <div className='md:pl-6 text-white z-[2]'>
                     {/* Mobile */}
-                    <div className='flex justify-center md:hidden pt-14'>
+                    <div className='flex justify-center md:hidden pt-36'>
                         <Image src='/assets/AhmedLinkedInPortrait.png' priority={true} width={200} height={200} alt='Ahmed Osman' className='imageportrait'/>
                     </div>
-                    <h2 className='justify-center w-[320px] md:justify-normal font-semibold heading text-5xl md:text-[54px] lg:text-[64px] xl:text-[68px] xl:w-[500px]'>{heading}</h2>
+                    <div className='flex md:inline justify-center md:justify-normal'>
+                        <h2 className='justify-center w-[320px] font-semibold heading text-[40px] min-[768px]:text-5xl min-[815px]:text-[54px] lg:text-[64px] xl:text-[68px] xl:w-[500px]'>{heading}</h2>
+                    </div>
                     <div className='pb-4 md:pb-8'>
-                        <ul className='flex'>
+                        <ul className='flex justify-center md:justify-normal'>
                             <li className='px-4'>
                                 <a href='https://www.linkedin.com/in/aosma/'>
                                     <Image src='/icons/socials/linkedinwhite.png' className='hover:brightness-90 transition' width={40} height={40} alt='LinkedIn Profile'/>
@@ -28,29 +30,31 @@ const Hero = ({heading, desc}) => {
                             </li>
                         </ul>
                     </div>
-                    <p className='py-5 inline-block font-thin w-[350px] desc leading-normal text-lg md:text-[24px] md:w-[360px] lg:text-[26px] lg:w-[410px] xl:text-[28px] xl:w-[450px]'>{desc}</p>
-                    <div className='justify-center pt-6'>
+                    <div className='flex text-center md:text-left justify-center md:justify-normal'>
+                        <p className='justify-center py-5 inline-block font-thin w-[338px] desc leading-normal text-lg md:text-[24px] md:w-[360px] lg:text-[26px] lg:w-[410px] xl:text-[28px] xl:w-[450px]'>{desc}</p>
+                    </div>
+                    <div className='pt-6 pb-20 md:pb-0'>
                         <ul>
-                            <li className='p-[6px]'>
-                                <button className='flex button_about transition-all hover:brightness-110' onClick={() => router.push('/about')}>
-                                    <a className='py-3 pl-[18px] pr-[64.35px]'>About</a>
-                                    <div className='py-3 pl-[110px]'>
+                            <li className='max-md:flex justify-center md:justify-normal p-[6px]'>
+                                <button className='flex max-md:w-[300px] max-md:h-[58px] button_about transition-all hover:brightness-110' onClick={() => router.push('/about')}>
+                                    <a className='py-3 pl-[18px] pr-[64.35px] max-md:text-2xl'>About</a>
+                                    <div className='py-3 pl-[100px] md:pl-[110px]'>
                                         <Image src='/icons/other/ArrowRight.svg' width={38} height={38} alt='Go' className='invert'/>
                                     </div>
                                 </button>
                             </li>
-                            <li className='p-[6px]'>
-                                <button className='flex button_exp_projects transition-all hover:brightness-125' onClick={() => router.push('/about#experience')}>
-                                <a className='py-3 pl-[18px] pr-[0.5px]'>Skills/Experience</a>
-                                    <div className='py-3 pl-[32px]'>
+                            <li className='max-md:flex justify-center md:justify-normal p-[6px]'>
+                                <button className='flex max-md:w-[300px] max-md:h-[58px] button_exp_projects transition-all hover:brightness-125' onClick={() => router.push('/about#experience')}>
+                                    <a className='py-3 pl-[18px] pr-[0.5px] max-md:text-2xl'>Skills/Experience</a>
+                                    <div className='py-3 pl-[42px] md:pl-[32px]'>
                                         <Image src='/icons/other/ArrowRight.svg' width={38} height={38} alt='Go' className='invert'/>
                                     </div>
                                 </button>
                             </li>
-                            <li className='p-[6px]'>
-                                <button className='flex button_exp_projects transition-all hover:brightness-125' onClick={() => router.push('/projects')}>
-                                <a className='py-3 pl-[18px] pr-[37.15px]'>Projects</a>
-                                    <div className='py-3 pl-[110px]'>
+                            <li className='max-md:flex justify-center md:justify-normal p-[6px]'>
+                                <button className='flex max-md:w-[300px] max-md:h-[58px] button_exp_projects transition-all hover:brightness-125' onClick={() => router.push('/projects')}>
+                                    <a className='py-3 pl-[18px] pr-[37.15px] max-md:text-2xl'>Projects</a>
+                                    <div className='py-3 pl-[104px] md:pl-[110px]'>
                                         <Image src='/icons/other/ArrowRight.svg' width={38} height={38} alt='Go' className='invert'/>
                                     </div>
                                 </button>
